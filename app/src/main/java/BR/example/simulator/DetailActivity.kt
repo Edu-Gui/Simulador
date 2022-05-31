a18cd78d6cfc643ec96c4ed48bdbd2a9fc724b45
edu-gui <e.guilherme100@gmail.com>
2022-05-31 12:02:50 -0300

    first commit

---
+++ app/src/main/java/BR/example/simulator/DetailActivity.kt
@@ -0,0 +1,19 @@
+package BR.example.simulator
+
+import BR.example.simulator.databinding.ActivityDetailBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
---
+++ app/src/main/java/BR/example/simulator/MainActivity.kt
@@ -0,0 +1,18 @@
+package BR.example.simulator
+
+import BR.example.simulator.databinding.ActivityMainBinding
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
